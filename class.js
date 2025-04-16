9120ae38590079220a414da0d2b7bf478d22cfcf
shrisubu <shubhamkumarstm14321@gmail.com>
2025-04-16 12:18:57 +0530

    add file

---
+++ class.js
@@ -0,0 +1,14 @@
+class classA
+{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+
+    }
+    display()
+    {
+        console.log(`person name is ${this.name} and age is ${this.age}`);
+    }
+}
+let myInfo=new classA("Ajay",23);
+myInfo.display();
---
+++ map.js
@@ -0,0 +1,9 @@
+const maps=new Map([
+    ["name","shubham"],
+    ["age","21"],
+    ["mob","9155859434"],
+    ["email","shubhamkumarstm14321@gmail.com"],
+    ["ismarried","false"]
+]
+);
+console.log(maps.get("name"));
